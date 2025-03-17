8c0860eafea16d8e4930025e77d847f56a7eb448
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-03-17 17:45:34 +0300

    update logout

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import NabbarButton from './NabbarButton'
+import { IoLogOut } from "react-icons/io5";
+import { useAuth } from '../Context/AuthContext';
+import { IconContext } from "react-icons";
 
 const NavBar = () => {
     const navbaritems = [
@@ -17,11 +20,30 @@ const NavBar = () => {
         }
 
     ]
+    const { user, logout } = useAuth();
+    const handleClick = () => {
+        logout();
+    }
+    console.log(user);
     return (
-        <nav className='border-[#d9d9d9] border-b-1 flex items-center justify-center font-main gap-32 py-7'>
-            {navbaritems.map((item, index) => (
-                <NabbarButton key={index} text={item.name} to={item.to} />
-            ))}
+        <nav className='border-[#919191] border-b-1 flex items-center'>
+            <div className='flex items-center justify-center font-main gap-32 py-7 flex-1'>
+                {navbaritems.map((item, index) => (
+                    <NabbarButton key={index} text={item.name} to={item.to} />
+                ))}
+
+            </div>
+            <div className='flex items-center gap-4  font-main'>
+                <p>{user.email}</p>
+                <button onClick={handleClick}>
+                    <IconContext.Provider value={{ color: "#919191" }}>
+                        <IoLogOut size={25} />
+                    </IconContext.Provider>
+
+                </button>
+
+            </div>
+
         </nav>
     )
 }
--- src/Pages/HomePageLayout.jsx
+++ src/Pages/HomePageLayout.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import { Outlet } from 'react-router'
 import NavBar from '../Components/NavBar'
 
+import { useAuth } from '../Context/AuthContext';
+
 const HomePageLayout = () => {
+
+
     return (
         <section className='w-screen h-screen overflow-x-hidden '>
             <div className='h-full px-3 md:px-5 pt-12 lg:px-40 '>
+
                 <NavBar />
                 <Outlet />
 
