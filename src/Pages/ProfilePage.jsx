35ccc4419e1a81d61d7bdffd0705600a301def41
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-03-17 15:47:09 +0300

    s

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useAuth } from '../Context/AuthContext'
 
 function ProfilePage() {
     const { refreshTokens } = useAuth();
+    useEffect(() => {
+        handleSubmit();
+    })
+
+    const handleSubmit = async () => {
+        try {
+            await refreshTokens();
+        } catch (error) {
+            alert('Ошибка!');
+        }
+    };
     return (
         <div>
             <button onClick={() => refreshTokens()} >refresh</button>
