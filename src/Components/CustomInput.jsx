bfd8cd09f564ef39ad4527808fb3ce8360f23f19
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-03-28 03:03:23 +0300

    updateInput

--- src/Components/CustomInput.jsx
+++ src/Components/CustomInput.jsx
@@ -5,7 +5,7 @@ function CustomInput({ placeholder, type, handleChange, label, value }) {
         <div className='flex flex-col gap-1.5'>
             <label className='font-main text-black'>{label}</label>
             <div className='w-full bg-[#f6f7fe] h-12 flex justify-center items-center px-3 border-1 border-[#d9d9d9] rounded-2xl'>
-                <input value={value ? value : ""} onChange={(e) => handleChange(e.target.value)} className='text-black w-full appearance-none outline-none border-none bg-transparent p-0 m-0 font-inherit ' placeholder={placeholder} type={type} />
+                <input value={value || ""} onChange={(e) => handleChange(e.target.value)} className='text-black w-full appearance-none outline-none border-none bg-transparent p-0 m-0 font-inherit ' placeholder={placeholder} type={type} />
             </div>
         </div>
 
