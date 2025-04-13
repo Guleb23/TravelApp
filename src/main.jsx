fc65796f602d01aa48fc2b44b1209bdfe1aee36b
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-04-13 04:12:52 +0300

    toaster

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import { AuthProvider } from './Context/AuthContext.jsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
 
